ffead55525c7931759e4c413f81bf7c4568ceb69
Fikir Demeke <fikirgenizele@gmail.com>
2026-02-24 15:11:36 -0500

    Initial Commit

---
+++ Soul FragmentsTests/Soul_FragmentsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Soul_FragmentsTests.swift
+//  Soul FragmentsTests
+//
+//  Created by Fikir  on 2/24/26.
+//
+
+import Testing
+@testable import Soul_Fragments
+
+struct Soul_FragmentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
