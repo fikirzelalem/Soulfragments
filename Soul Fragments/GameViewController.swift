60823edfb13351b86f7cb31d7f83d5244e0142ed
Fikir Demeke <fikirgenizele@gmail.com>
2026-03-17 12:24:46 -0400

    feat: replaced Xcode template with real game scene

--- Soul Fragments/GameViewController.swift
+++ Soul Fragments/GameViewController.swift
@@ -2,115 +2,207 @@
 //  GameViewController.swift
 //  Soul Fragments
 //
-//  Created by Fikir  on 2/24/26.
-//
 
 import UIKit
-import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
 
+    // MARK: - Properties
+
+    let gameState = GameState()
+
+    var scnView: SCNView!
+    var scene: SCNScene!
+
+    var playerNode: SCNNode!
+    var cameraNode: SCNNode!
+    var cameraArm: SCNNode!   // pivot that follows the player
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
+
+        setupView()
+        setupScene()
+        setupGround()
+        setupObstacles()
+        setupPlayer()
+        setupCamera()
+        setupLighting()
+
+        gameState.load()
+        gameState.startGame()
+    }
+
+    // MARK: - View
+
+    private func setupView() {
+        scnView = self.view as! SCNView
         scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        scnView.antialiasingMode = .multisampling4X
+        scnView.allowsCameraControl = false
     }
-    
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
+
+    // MARK: - Scene
+
+    private func setupScene() {
+        scene = SCNScene()
+        scnView.scene = scene
+        applyDimensionAtmosphere(gameState.currentDimension)
     }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
+
+    private func applyDimensionAtmosphere(_ dimension: Dimension) {
+        scene.fogColor = dimension.fogColor
+        scene.fogStartDistance = 18
+        scene.fogEndDistance = 45
+        scene.background.contents = UIColor(red: 0.02, green: 0.0, blue: 0.05, alpha: 1.0)
     }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
+
+    // MARK: - Ground
+
+    private func setupGround() {
+        let floor = SCNFloor()
+        floor.reflectivity = 0.08
+        floor.reflectionFalloffEnd = 6
+
+        let mat = SCNMaterial()
+        mat.diffuse.contents = UIColor(red: 0.08, green: 0.04, blue: 0.12, alpha: 1.0)
+        mat.roughness.contents = 1.0
+        floor.materials = [mat]
+
+        let groundNode = SCNNode(geometry: floor)
+        groundNode.name = "ground"
+        groundNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        scene.rootNode.addChildNode(groundNode)
+    }
+
+    // MARK: - Obstacles
+
+    private func setupObstacles() {
+        let layout: [(SCNVector3, Float)] = [
+            (SCNVector3( 5, 0,  -4), 1.2),
+            (SCNVector3(-4, 0,  -6), 1.6),
+            (SCNVector3( 3, 0,   6), 1.0),
+            (SCNVector3(-7, 0,   3), 2.0),
+            (SCNVector3( 8, 0,   1), 0.9),
+            (SCNVector3(-2, 0,  -9), 1.4),
+        ]
+
+        for (pos, size) in layout {
+            let box = SCNBox(
+                width:  CGFloat(size),
+                height: CGFloat(size * 2.2),
+                length: CGFloat(size),
+                chamferRadius: 0.04
+            )
+            let mat = SCNMaterial()
+            mat.diffuse.contents  = UIColor(red: 0.12, green: 0.06, blue: 0.18, alpha: 1.0)
+            mat.specular.contents = UIColor(red: 0.3,  green: 0.1,  blue: 0.5,  alpha: 1.0)
+            mat.roughness.contents = 0.85
+            box.materials = [mat]
+
+            let node = SCNNode(geometry: box)
+            node.name = "obstacle"
+            node.position = SCNVector3(pos.x, size * 1.1, pos.z)
+            node.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+            scene.rootNode.addChildNode(node)
         }
     }
 
+    // MARK: - Player
+
+    private func setupPlayer() {
+        let capsule = SCNCapsule(capRadius: 0.35, height: 1.5)
+        let mat = SCNMaterial()
+        mat.diffuse.contents  = UIColor(red: 0.7,  green: 0.15, blue: 0.95, alpha: 1.0)
+        mat.emission.contents = UIColor(red: 0.35, green: 0.0,  blue: 0.55, alpha: 1.0)
+        mat.roughness.contents = 0.4
+        capsule.materials = [mat]
+
+        playerNode = SCNNode(geometry: capsule)
+        playerNode.name = "player"
+        playerNode.position = SCNVector3(0, 0.75, 0)
+
+        let shape = SCNPhysicsShape(geometry: capsule, options: nil)
+        playerNode.physicsBody = SCNPhysicsBody(type: .kinematic, shape: shape)
+
+        // Glow point light attached to player
+        let glow = SCNLight()
+        glow.type = .omni
+        glow.color = UIColor(red: 0.6, green: 0.0, blue: 1.0, alpha: 1.0)
+        glow.intensity = 250
+        glow.attenuationStartDistance = 0.5
+        glow.attenuationEndDistance = 5
+
+        let glowNode = SCNNode()
+        glowNode.light = glow
+        playerNode.addChildNode(glowNode)
+
+        scene.rootNode.addChildNode(playerNode)
+    }
+
+    // MARK: - Camera
+
+    private func setupCamera() {
+        // Arm anchored to player position, camera sits behind + above
+        cameraArm = SCNNode()
+        cameraArm.position = playerNode.position
+        scene.rootNode.addChildNode(cameraArm)
+
+        let cam = SCNCamera()
+        cam.zNear = 0.1
+        cam.zFar  = 120
+        cam.fieldOfView = 68
+
+        cameraNode = SCNNode()
+        cameraNode.camera = cam
+        cameraNode.position = SCNVector3(0, 5, 11)
+
+        let lookAt = SCNLookAtConstraint(target: playerNode)
+        lookAt.isGimbalLockEnabled = true
+        cameraNode.constraints = [lookAt]
+
+        cameraArm.addChildNode(cameraNode)
+        scnView.pointOfView = cameraNode
+    }
+
+    // MARK: - Lighting
+
+    private func setupLighting() {
+        // Directional "moonlight"
+        let moon = SCNLight()
+        moon.type = .directional
+        moon.color = UIColor(red: 0.35, green: 0.35, blue: 0.75, alpha: 1.0)
+        moon.intensity = 900
+        moon.castsShadow = true
+        moon.shadowMode  = .deferred
+        moon.shadowColor = UIColor(white: 0, alpha: 0.75)
+        moon.shadowRadius = 4
+        moon.shadowSampleCount = 8
+
+        let moonNode = SCNNode()
+        moonNode.light = moon
+        moonNode.eulerAngles = SCNVector3(-Float.pi / 3, Float.pi / 4, 0)
+        scene.rootNode.addChildNode(moonNode)
+
+        // Dark ambient
+        let ambient = SCNLight()
+        ambient.type = .ambient
+        ambient.color = UIColor(red: 0.04, green: 0.0, blue: 0.08, alpha: 1.0)
+        ambient.intensity = 350
+
+        let ambientNode = SCNNode()
+        ambientNode.light = ambient
+        scene.rootNode.addChildNode(ambientNode)
+    }
+
+    // MARK: - System
+
+    override var prefersStatusBarHidden: Bool { true }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        UIDevice.current.userInterfaceIdiom == .phone ? .allButUpsideDown : .all
+    }
 }
